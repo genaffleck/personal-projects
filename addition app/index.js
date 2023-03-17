87f74f3c4f149553db40cbce3d1927bd66f7c139
genaffleck <97855765+genaffleck@users.noreply.github.com>
2023-03-18 02:20:34 +0800

    more practice

--- addition app/index.js
+++ addition app/index.js
@@ -1,5 +1,6 @@
 const num1 = Math.ceil(Math.random()*21)-10;
 const num2 = Math.ceil(Math.random()*21)-10;
+const chosenValue = Math.random() < 0.5 ? "+" : "-";
 
 
 const questionEl = document.getElementById("question")
@@ -19,21 +20,45 @@ if(!score){
 
 scoreEl.innerText = `score: ${score}`
 
-questionEl.innerText = `What is the sum of ${num1} and ${num2}?`;
+if(num2<0 && chosenValue==="-"){
+  questionEl.innerText = `Evaluate: ${num1} ${chosenValue} (${num2})?`;
+}else{
+  questionEl.innerText = `Evaluate: ${num1} ${chosenValue} ${num2}?`;
+}
+;
 
-const correctAns = num1 + num2;
+if(num2 <0 && chosenValue==="+"){
+  questionEl.innerText = `Evaluate: ${num1} ${chosenValue} (${num2})?`;
+};
 
-formEl.addEventListener("submit", ()=>{
-  const userAns = +inputEl.value
-  if(userAns===correctAns){
-    score++
-    updateLocalStorage()
-  }else {
-    score--
-    updateLocalStorage()
-    // localStorage.clear() //placing this here sets local storage to zero when user gives incorrect answer
-  }
-});
+if(chosenValue === "+"){
+  const correctAns = num1 + num2;
+  formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value
+    if(userAns===correctAns){
+      score++
+      updateLocalStorage()
+    }else {
+      score--
+      updateLocalStorage()
+      // localStorage.clear() //placing this here sets local storage to zero when user gives incorrect answer
+    }
+  });
+}else{
+  const correctAns = num1 - num2;
+  formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value
+    if(userAns===correctAns){
+      score++
+      updateLocalStorage()
+    }else {
+      score--
+      updateLocalStorage()
+      // localStorage.clear() //placing this here sets local storage to zero when user gives incorrect answer
+    }
+  });
+};
+
 
 function updateLocalStorage(){
   localStorage.setItem("score", JSON.stringify(score))
